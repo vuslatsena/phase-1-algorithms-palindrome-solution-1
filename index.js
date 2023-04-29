40fafb0ea59fc1bdd1210067e1aae0486a503acd
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 10:05:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Remove any non-alphanumeric characters and convert to lowercase
+  let cleaned = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  // Reverse the cleaned string
+  let reversed = reverseString(cleaned);
+
+  // Compare the cleaned string to the reversed string
+  return cleaned === reversed;
+}
+
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 }
 
 /* 
@@ -14,12 +29,26 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+console.log(isPalindrome("racecar")); // true
 
-  console.log("");
+console.log("Expecting: false");
+console.log(isPalindrome("robot")); // false
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+console.log("Expecting: true");
+console.log(isPalindrome("A man a plan a canal Panama")); // true
+
+console.log("Expecting: true");
+console.log(isPalindrome("12321")); // true
+
+console.log("Expecting: true");
+console.log(isPalindrome("deified")); // true
+
+console.log("Expecting: false");
+console.log(isPalindrome("not a palindrome")); // false
+
+// Custom test case
+console.log("Expecting: true");
+console.log(isPalindrome("tattarrattat"));
 }
 
 module.exports = isPalindrome;
